651834368f42e29fafad937d118324d7e503ea16
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-08-19 20:17:12 +0500

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => { 
  try {
-  const conn = await mongoose.connect(process.env.MONGO_URI)
+  const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   return true
  } catch (error) {
   console.log(error);
-  process.exit(1)
-  return false
+  process.exit(1); 
  }
 }
 
